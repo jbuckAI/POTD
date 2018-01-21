550fb2208af9b0f13e3de1ef9edae181de80989d
jbuckAI <34896844+jbuckAI@users.noreply.github.com>
2018-01-21 10:29:11 -0600

    update backgroun
    provided an example goes_east.jpg and code that makes this picture your desktop backgroun

--- POTD/POTD/POTD.py
+++ POTD/POTD/POTD.py
@@ -1,1 +1,14 @@
+#update the desktop background
+#https://stackoverflow.com/questions/40941167/changing-desktop-background-in-windows-10-via-python
 
+import ctypes
+
+directory = "C:\Users\joshua.buck\Documents\GitHub\POTD\POTD\POTD"
+imagePath = directory + "\goes_east.jpg"
+
+def changeBG(imagePath):
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imagePath , 3)
+    return;
+
+changeBG(imagePath)
